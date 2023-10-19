562b7e824765b9c5c91ae89a1607516ae462455b
DenisUrev01 <erikonson2@bk.ru>
2023-10-19 16:24:41 +0300

    6.2

---
+++ 6_sem_dz/zadacha_42/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите значение k1: ");
+        double k1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение b1: ");
+        double b1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение k2: ");
+        double k2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение b2: ");
+        double b2 = double.Parse(Console.ReadLine());
+
+        // Находим x-координату точки пересечения
+        double x = (b2 - b1) / (k1 - k2);
+
+        // Вычисляем y-координату точки пересечения
+        double y = k1 * x + b1;
+
+        Console.WriteLine($"Точка пересечения: ({x}, {y})");
+    }
+}
