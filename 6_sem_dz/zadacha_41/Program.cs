17a93913e1cb4583d9af65b1353118a5535f0550
DenisUrev01 <erikonson2@bk.ru>
2023-10-19 16:13:17 +0300

    6.1

---
+++ 6_sem_dz/zadacha_41/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите числа через запятую: ");
+        string input = Console.ReadLine();
+        string[] numberStrings = input.Split(',');
+        int count = 0;
+
+        foreach (string numStr in numberStrings)
+        {
+            if (int.TryParse(numStr, out int number) && number > 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"Количество чисел больше 0: {count}");
+    }
+}
+
